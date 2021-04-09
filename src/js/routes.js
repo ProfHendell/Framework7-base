8785e39fe2e66e389559b8bca955f4e1a98c3e08
MrHendell <hendelporras@hotmail.com>
2021-04-09 13:37:08 -0600

    Agregado registro de usaurio

--- src/js/routes.js
+++ src/js/routes.js
@@ -9,10 +9,16 @@ import DynamicRoutePage from '../pages/dynamic-route.f7.html';
 import RequestAndLoad from '../pages/request-and-load.f7.html';
 import NotFoundPage from '../pages/404.f7.html';
 
+import RegistrarPage from '../pages/registrar.f7.html';
+
 var routes = [
   {
     path: '/',
     component: HomePage,
+  },
+  {
+    path: '/registrar-page/',
+    component: RegistrarPage,
   },
   {
     path: '/about/',
